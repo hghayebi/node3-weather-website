433be0c59c5ded3dba611624c6589d073c1b50c1
Hossein ghayebi <nhosseinghayebi@gmail.com>
2021-08-17 20:41:26 +0430

    Remove unnecessary consol.logs

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,16 @@
-// console.log("client side javascript file loaded");
-
-// const loc = ["Ardabil", "Parsabad moghan", "Tehran", "Tabriz", "Qazvin iran"];
 
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
 const messageTwo = document.querySelector("#message-2");
 
-// messageOne.textContent = "From javascript";
+
 
 weatherForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const location = search.value;
-  // console.log(location);
+ 
   search.value = "";
 
   messageOne.textContent = "loading...";
